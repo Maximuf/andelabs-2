9349e41616310994cf8e19ffda15ed681bf0d3eb
Solomon Kingsley <johnkingzy@gmail.com>
2017-02-14 13:38:49 +0100

    I just push all elements in the array into the obj

--- app/words.js
+++ app/words.js
@@ -6,6 +6,9 @@ const words = {
   const repl = str.replace(/[\n\t]/g, " " );
   //Split every word by a space
   repl = repl.split(" ");
-  
+  //Push every element in the array into the obj
+  for(var i in repl){
+    obj[repl[i]] = 1;
+  }
 }
 }
