c2cedd890d5371baf02328264ce2f22dbf0075e2
Solomon Kingsley <johnkingzy@gmail.com>
2017-02-14 13:35:09 +0100

    I just wrote a function to splite evry word by a space

---
+++ app/words.js
@@ -0,0 +1,11 @@
+'use strict'
+const words = {
+  words: function(str){
+  const obj = {};
+  //Replace every \n \t with a space
+  const repl = str.replace(/[\n\t]/g, " " );
+  //Split every word by a space
+  repl = repl.split(" ");
+  
+}
+}
